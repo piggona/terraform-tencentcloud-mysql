e2e39ce4a63b6635dc5d5c0fb54213c560f78909
yuanhaojin <yuanhaojin@tencent.com>
2020-07-17 17:31:02 +0800

    initialize mysql module

---
+++ examples/privilege/main.tf
@@ -0,0 +1,53 @@
+variable "region" {
+    default = "ap-guangzhou"
+}
+
+provider "tencentcloud" {
+    version = ">=1.18.1"
+    region = "ap-guangzhou"
+}
+
+module "privilege" {
+    source = "../../"
+
+    create_new = false
+    instance_id = "cdb-rslbpa09"
+    account = [
+        {
+            account_name = "test_account"
+            account_password = "test_passwd1"
+        },
+        {
+            account_name = "test_account1"
+            account_password = "test_passwd2"
+        }
+    ]
+    mysql_privilege = [
+        {
+            account_name = "test_account"
+            global = ["SELECT"]
+            database = [
+                {
+                    database_name = "my_database"
+                    privileges = ["INSERT","UPDATE"]
+                }
+            ]
+            table = [
+                {
+                    database_name = "my_database"
+                    table_name = "my_table"
+                    privileges = ["DELETE"]
+                }
+            ]
+            column = [
+                {
+                    database_name = "my_database"
+                    table_name = "my_table"
+                    column_name = "test_column"
+                    privileges = ["UPDATE"]
+                }
+            ]
+
+        }
+    ]
+}
---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+    region = var.region != "" ? var.region : null
+    availability_zone = var.availability_zone != "" ? var.availability_zone : "ap-guangzhou-2"
+    account_number = !var.create_new && var.instance_id == "" ? 0 : length(var.account)
+    account_privilege_number = var.create_new || var.instance_id != "" ? length(var.account_privilege) : 0
+    mysql_privilege_number = var.create_new || var.instance_id != "" ? length(var.mysql_privilege) : 0
+    readonly_instance_number = var.create_new || var.instance_id != "" ? length(var.readonly_instances) : 0
+}
